2a2f63415dac18d8796061f074fc56715c291f14
Ayaz Uddin <ayaz.din@gmail.com>
2018-03-16 12:46:37 +0545

    admin initialized

---
+++ admin/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Models\Auth\User;
+
+class AuthController extends Controller
+{
+    public function _construct(){
+        $this->middleware('guest',['except'=>'getLogout']);
+    }
+    public function authenticate(Request $request){
+        $credentials=$request->only('email','password');
+
+        try{
+            if(! $token = JWTAuth::attempt($credentials)){
+                return $this->response->error(['error'=>'user credentials are not correct'],401);
+            }
+
+        }catch(JWTException $ex){
+            return $this->response->error(['error'=>'something went wrong'],500);
+        }
+
+        return $this->response->array(compact('token'))->setStatusCode(200);
+    }
+    protected function create(array $data)
+    {
+        return User::create([
+            'name'=>$data['name'],
+            'email'=>$data['email'],
+            'password'=>bcrypt($data['password']),
+        ]);
+    }
+
+}
