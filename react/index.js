d6739a6f8dd227ea6273ce7868df47a199a30efe
anita <anitalama07@gmail.com>
2018-03-20 15:21:31 +0545

    react files

--- react/index.js
+++ react/index.js
@@ -1,20 +1,4 @@
-import React, { Component } from 'react';
-import { AppRegistry, View, Text, ScrollView } from 'react-native';
-
+import { AppRegistry } from 'react-native';
 import App from './App';
-{/*
-export default class apple extends Component<Props> {
-  render() {
-    return (
-      <View>
-      	<Text>APP</Text>
-      	<App />
-       	
-      </View>
-    );
-  }
-}
-*/}
 
-
-AppRegistry.registerComponent('wine', () => App);
+AppRegistry.registerComponent('restro', () => App);
